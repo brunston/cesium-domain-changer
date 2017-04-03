cf9aba49bab6682566287d82d13c8774923eabd4
Brunston Poon <albatrossian@outlook.com>
2017-04-03 16:35:51 -0700

    cleaned up code

--- yedit.py
+++ yedit.py
@@ -6,11 +6,6 @@ import fileinput
 import re
 import sys
 
-#try:
-##    #yaml = open('docker-compose.yml', 'w')
-#except FileNotFoundError:
-#    sys.exit("YAML file docker-compose.yml not found")
-
 # get the information from the conf file
 conf = open('fields.conf', 'r')
 domain = conf.readline().rstrip()
@@ -21,5 +16,4 @@ for line in fileinput.input(inplace=1, backup='.bak'):
     line = re.sub('test@example.com', email, line.rstrip())
     line = re.sub('example.com', domain, line.rstrip())
     print(line)
-#yaml.close()
 
